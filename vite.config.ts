2fcd11f0648c562777172c8e2befefdfcb964dcf
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2024-03-03 22:30:11 +0100

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.BUILD_TARGET === 'gh-pages'
+  ? '/genebike'
+  : '/',
   plugins: [
     vue(),
   ],
