8e041ee54e50b2fce605813bc4df6bcbac77a98b
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2024-03-05 21:50:18 +0100

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,12 @@ export default defineConfig({
   : '/',
   plugins: [
     vue(),
+    VitePWA({
+       registerType: 'autoUpdate',
+       workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
+      },
+     })
   ],
   resolve: {
     alias: {
