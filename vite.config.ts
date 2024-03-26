a07006a1db36e3de168dcc62c20ca75e20e3f953
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2024-03-26 22:04:38 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.BUILD_TARGET === 'gh-pages'
-  ? '/genebike'
+  ? '/'
   : '/',
   plugins: [
     vue(),
